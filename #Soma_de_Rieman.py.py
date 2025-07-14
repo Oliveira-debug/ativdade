72a637f3e7a20997ab7b99ef8b7a4a9f824e9bc9
Natanael de oliveira <notebook.chan.69@gmail.com>
2025-07-14 14:57:06 -0300

    Add files via upload

---
+++ #Soma_de_Rieman.py.py
@@ -0,0 +1,25 @@
+# Soma de Reiman
+# Bibliotecas importadas para o código
+from sympy import integrate, symbols
+#F(x)
+x = symbols('x')
+função = (x**2)+(3*x)+10 
+#intervalo
+xinicial = 1
+xfinal = 50
+Soma_de_Reiman = integrate(função, (x, xinicial, xfinal))
+# Integração simbólica exata
+integral_definida = integrate(função, (x, xinicial, xfinal))
+print("Integral definida (valor exato):", integral_definida)
+# Número de subintervalos
+n = 100
+dx = (xfinal - xinicial) / n
+# Pontos à esquerda dos subintervalos
+def f(x):
+    return x**2 + 3*x + 10
+soma_riemann = 0
+for i in range(n):
+    xi = xinicial + i * dx  # ponto à esquerda de cada subintervalo
+    soma_riemann += f(xi) * dx
+print("Aproximação por Soma de Riemann (esquerda):", soma_riemann)
+print("Integral Definida=")
